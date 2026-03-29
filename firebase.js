5fa31f906e899bc03323a6572844c39a9719b2b0
jawsatak47 <jawsatak47@gmail.com>
2026-03-29 11:04:59 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,8 +7,8 @@ const firebaseConfig = {
   projectId: "canvaswars541",
   storageBucket: "canvaswars541.firebasestorage.app",
   messagingSenderId: "536740276923",
-  appId: "1:536740276923:web:cf29459d6cde9fc2fb579e",
-  measurementId: "G-JEG4SRT6YF"
+  appId: "1:536740276923:web:73a0635344efb6d3fb579e",
+  measurementId: "G-NPL4RVL4SJ"
 };
 
 // This line "turns on" the connection
@@ -16,3 +16,8 @@ const app = initializeApp(firebaseConfig);
 
 // This line makes the Database available to your other files
 export const db = getFirestore(app);
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  
